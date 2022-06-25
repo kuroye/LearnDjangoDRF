f18e81c83dda76477db05a882876268768d08220
kuro_ye <erhanaket77@gmail.com>
2022-06-25 21:39:26 +0600

    add urls related to user

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,11 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
+
     re_path(r'^articles/$', views.ArticleList.as_view()),
-    re_path(r'^articles/(?P<pk>[0-9]+)$', views.ArticleDetails.as_view()),]
+    re_path(r'^articles/(?P<pk>[0-9]+)$', views.ArticleDetails.as_view()),
+    re_path(r'^users/$', views.ArticleList.as_view()),
+    re_path(r'^users/(?P<pk>[0-9]+)$', views.ArticleDetails.as_view()),
+]
 
 urlpatterns = format_suffix_patterns(urlpatterns)

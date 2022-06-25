6b8b577f98d4b5207c72f24693d8077ecab0f9be
kuro_ye <erhanaket77@gmail.com>
2022-06-25 20:58:24 +0600

    get cn_status

--- blog/serializers.py
+++ blog/serializers.py
@@ -4,11 +4,21 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class ArticleSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
     full_status = serializers.ReadOnlyField(source='get_status_display')
+    cn_status = serializers.SerializerMethodField()
 
     class Meta:
         model = Article
         fields = '__all__'
         read_only_fields = ('id', 'author', 'create_date')
+
+    def get_cn_status(self, obj):
+        if obj.status == 'p':
+            return '已发表'
+        elif obj.status == 'd':
+            return '草稿'
+        else:
+            return ''
